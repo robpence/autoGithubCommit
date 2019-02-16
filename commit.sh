440bd31ebb37d2b025cf82ac6804e1c7ef847eca
Robert Pence <rpence12395@gmail.com>
2019-02-16 15:52:15 -0800

    Does this work now?

--- commit.sh
+++ commit.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 echo "Hello running cronjob..."
-echo "Hello" >> whatever.txt
-git commit -a -m "This is a commit message."
-git push
+echo Hello >> whatever.txt
+git commit -a -m "Does this work now?"
